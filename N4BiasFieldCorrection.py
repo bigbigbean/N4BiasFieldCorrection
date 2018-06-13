8386dcd2587c60bf8f028c3bdaa4ae9fc0a0f42f
bigbigbean <33394074+bigbigbean@users.noreply.github.com>
2018-06-13 13:48:14 +0800

    Update N4BiasFieldCorrection.py

--- N4BiasFieldCorrection.py
+++ N4BiasFieldCorrection.py
@@ -4,7 +4,7 @@ def N4():
     print("N4 bias correction runs.")
     inputImage = sitk.ReadImage("06-t1c.nii.gz")
     # maskImage = sitk.ReadImage("06-t1c_mask.nii.gz")
-    maskImage = sitk.OtsuThreshold(inputImage,0,1,35)
+    maskImage = sitk.OtsuThreshold(inputImage,0,1,200)
     sitk.WriteImage(maskImage, "06-t1c_mask3.nii.gz")
 
     inputImage = sitk.Cast(inputImage,sitk.sitkFloat32)
